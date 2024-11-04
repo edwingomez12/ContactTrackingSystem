1bb88eaa399344e1a97942b501c1871e00192aea
edwingomez12 <106348997+edwingomez12@users.noreply.github.com>
2024-11-03 21:10:14 -0600

    Add more tests

--- CandidateTracking/UnitTests/CandidateModelTests.cs
+++ CandidateTracking/UnitTests/CandidateModelTests.cs
@@ -76,4 +76,57 @@ public class CandidateModelTests
         
         validationResults.Should().Contain(v => v.MemberNames.Contains("ResidentialZipCode"));
     }
+    
+    [Theory]
+    [InlineData("123-456-7890", true)]
+    [InlineData("1234567890", true)]
+    [InlineData("123-4567-890", true)]
+    [InlineData("(123)-456-7890", true)]
+    [InlineData("123-456-789a", false)]
+    public void PhoneNumber_ShouldBeInCorrectFormat(string phoneNumber, bool isValid)
+    {
+        // Arrange
+        var candidate = new Candidate
+        {
+            FirstName = "test",
+            LastName = "test1",
+            EmailAddress = "test@gmail.com",
+            PhoneNumber = phoneNumber,
+            ResidentialZipCode = "78577"
+        };
+        var validationResults = new List<ValidationResult>();
+        var validationContext = new ValidationContext(candidate);
+
+        // Act
+        var result = Validator.TryValidateObject(candidate, validationContext, validationResults, true);
+
+        // Assert
+        result.Should().Be(isValid);
+    }
+    
+    [Theory]
+    [InlineData("78577", true)]
+    [InlineData("785", false)]
+    [InlineData("785-77", false)]
+    [InlineData("abcd", false)]
+    public void ZipCode_ShouldBeInCorrectFormat(string zipCode, bool isValid)
+    {
+        // Arrange
+        var candidate = new Candidate
+        {
+            FirstName = "test",
+            LastName = "test1",
+            EmailAddress = "test@gmail.com",
+            PhoneNumber = "123457897",
+            ResidentialZipCode = zipCode
+        };
+        var validationResults = new List<ValidationResult>();
+        var validationContext = new ValidationContext(candidate);
+
+        // Act
+        var result = Validator.TryValidateObject(candidate, validationContext, validationResults, true);
+
+        // Assert
+        result.Should().Be(isValid);
+    }
 }
